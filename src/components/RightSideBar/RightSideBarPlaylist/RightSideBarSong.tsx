3a14ff83d335936880a1bb97ebde3d2eb685d7f8
Mario semeraro <business@viraluos.com>
2025-04-25 02:15:58 +0200

    `Update RightSideBarSong component with new styles and functionality`

--- src/components/RightSideBar/RightSideBarPlaylist/RightSideBarSong.tsx
+++ src/components/RightSideBar/RightSideBarPlaylist/RightSideBarSong.tsx
@@ -5,16 +5,18 @@ import { usePlayerStore } from "@/stores/usePlayerStore";
 export default function LeftSideBarSong({ song, index }: { song: SidebarSong, index: number }) {
     const playSong = usePlayerStore((state) => state.playSong);
 
+    const { currentIndex } = usePlayerStore();
+
     return (
         <li
-            className="flex gap-2 p-2 hover:bg-gray-700/50 rounded cursor-pointer transition"
+            className={`${index == currentIndex ? "bg-gray-700" : ""} flex gap-2 p-2 hover:bg-gray-700/50 rounded cursor-pointer transition`}
             onClick={() => playSong(song, index)}
         >
             <Image
-                className="w-10 h-10 rounded-md object-cover"
+                className="w-14 h-14 rounded-md object-cover"
                 alt={song.title}
                 src={song.image}
-                width={40} height={40}
+                width={60} height={60}
             />
             <div className="flex flex-col w-64">
                 <span className="block text-lg font-medium truncate">{song.title}</span>
